ed3da4fb25b8d9e8e8f78ef9bb79edb8886697c5
CSEsubham <mutnurisubham@gmail.com>
2025-07-08 14:51:07 +0530

    intial form

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='bg-white text-rose-500 flex justify-center items-center h-[100vh]'>hello</h1>
+    </div>
+  )
+}

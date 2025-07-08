fcaad9fca69ff65190f47aa33d34b4f20a2ab6e2
CSEsubham <mutnurisubham@gmail.com>
2025-07-08 15:57:24 +0530

    componets

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <h1 className='bg-white text-rose-500 flex justify-center items-center h-[100vh]'>hello</h1>
+      <h1 className='bg-white text-4xl text-rose-500 flex justify-center items-center h-[100vh] '>hello</h1>
     </div>
   )
 }
